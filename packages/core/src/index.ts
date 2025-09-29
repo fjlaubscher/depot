bc3eea9aca3df1dd8e29f27e9de64475ea7b9d80
Francois Laubscher <francois.laubscher@toptal.com>
2025-09-29 16:41:57 +0200

    Fix tsconfig.json to always produce ESM

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
 // Export all depot types
-export * as depot from './types/depot';
+export * as depot from './types/depot.js';
 
 // Export slug utilities
-export * as slug from './utils/slug';
+export * as slug from './utils/slug.js';
 
 // Export all wahapedia types
-export * as wahapedia from './types/wahapedia';
+export * as wahapedia from './types/wahapedia.js';
