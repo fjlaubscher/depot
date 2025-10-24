80fef185252433acb0c21e2c91ad9dc7eaa35594
Francois Laubscher <francoisjlaubscher@gmail.com>
2025-10-24 14:27:27 +0200

    fix: pwa asset icons

--- packages/web/pwa-assets.config.ts
+++ packages/web/pwa-assets.config.ts
@@ -3,7 +3,22 @@ import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/conf
 export default defineConfig({
   preset: {
     ...minimal2023Preset,
+    transparent: {
+      ...minimal2023Preset.transparent,
+      resizeOptions: {
+        background: '#ff6900',
+        fit: 'contain'
+      }
+    },
+    apple: {
+      ...minimal2023Preset.apple,
+      resizeOptions: {
+        background: '#ff6900',
+        fit: 'contain'
+      }
+    },
     maskable: {
+      ...minimal2023Preset.maskable,
       sizes: [512],
       resizeOptions: {
         background: '#ff6900', // Match your theme color
