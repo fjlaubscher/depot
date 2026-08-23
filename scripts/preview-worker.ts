4440ad92f9d0e6b67139e83f8800a2c49d79d1c3
Francois Laubscher <francoisjlaubscher@gmail.com>
2026-08-23 17:27:17 +0000

    chore(ci): deploy PR previews to Pages instead of a Worker

--- scripts/preview-worker.ts
+++
@@ -1,9 +0,0 @@
-import { onRequest } from '../functions/[[path]].ts';
-
-type Env = { ASSETS: { fetch: (request: Request) => Promise<Response> } };
-
-export default {
-  fetch(request: Request, env: Env): Promise<Response> {
-    return onRequest({ request, env });
-  }
-};
